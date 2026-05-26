be6630944d4762e75a265f19460378475377327c
ABISHEK007-R <abishek011206abi@gmail.com>
2026-05-26 12:16:46 +0530

    Update CenterNotFoundException.java

--- CenterNotFoundException.java
+++ CenterNotFoundException.java
@@ -1,12 +1,9 @@
 package com.wipro.cws.util;
 
 public class CenterNotFoundException extends Exception {
-
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
+   public CenterNotFoundException(String Message){
+    super(Message);
+   }
 	@Override
     public String toString() {
         return "CenterNotFoundException: Service center does not exist!";
