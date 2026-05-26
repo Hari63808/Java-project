c757d66a32eee485b5ee6be3205ad1b407b78e9f
ABISHEK007-R <abishek011206abi@gmail.com>
2026-05-26 12:19:16 +0530

    Update SlotUnavailableException.java

--- SlotUnavailableException.java
+++ SlotUnavailableException.java
@@ -1,12 +1,9 @@
 package com.wipro.cws.util;
 
 public class SlotUnavailableException extends Exception {
-
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
+ public SlotUnavailableException(String Message) {
+	 super(Message);
+ }
 	@Override
     public String toString() {
         return "SlotUnavailableException: Selected slot is already booked!";
